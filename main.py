d761f68626e14ff245f0188cb33313c8f204182e
saanoojj <95056798+saanoojj@users.noreply.github.com>
2024-12-03 11:29:58 +0100

    fixed passing "Bundesland"

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from modules.modules import create_monthly_schedule
 @click.option('--gehalt', '-g', type=float, default=12.50, help='Stundenlohn (Standard: 12.50)')
 @click.option('--bundesland', '-bl', default='SN', help='Auswahl des Bundeslandes (für die Feiertage), Standard ist Sachsen (1)')
 
-def main(dateiname, zuschlag, gehalt, bundesland):
+def main(dateiname, bundesland, zuschlag, gehalt):
     
-    erstellte_datei = create_monthly_schedule(dateiname, zuschlag, gehalt)
+    erstellte_datei = create_monthly_schedule(dateiname, bundesland, zuschlag, gehalt)
     print(f"Excel-Datei wurde erstellt: {erstellte_datei}")
 
 if __name__ == "__main__":
